119549ce4b1e83067aae28d881b00a5154d2b549
Yifang Ma <yifang@nearprotocol.com>
2020-07-26 16:51:32 -0700

    add useSubstrate

---
+++ src/substrate-lib/utils.js
@@ -0,0 +1,13 @@
+const utils = {
+  paramConversion: {
+    num: [
+      'Compact<Balance>',
+      'BalanceOf',
+      'TokenBalance',
+      'u8', 'u16', 'u32', 'u64', 'u128',
+      'i8', 'i16', 'i32', 'i64', 'i128'
+    ]
+  }
+};
+
+export default utils;
