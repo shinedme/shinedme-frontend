df74d753793e3c10564c2c400f9afe2c098cdcd4
Bo Yao <bo@nearprotocol.com>
2020-08-14 13:25:38 -0700

    typo

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import configCommon from './common.json';
 // Using `require` as `import` does not support dynamic loading (yet).
 const configEnv = require(`./${process.env.NODE_ENV}.json`);
 if (process.env.BLOCKCHAIN_URL) {
-  configEnv.PROVIDER_SOCKET = node.process.BLOCKCHAIN_URL;
+  configEnv.PROVIDER_SOCKET = process.env.BLOCKCHAIN_URL;
 }
 
 // Accepting React env vars and aggregating them into `config` object.
